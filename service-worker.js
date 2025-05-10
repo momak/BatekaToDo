4a5d05a1c1aa3b29a0318aa2b1ece2f7e5f22799
Bojan Bogdanovski <bbogdanovski@hotmail.com>
2025-05-11 00:27:58 +0200

    publish v0.9.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: uIrCttap */
+/* Manifest version: epH3LQRE */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
