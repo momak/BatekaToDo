995873cfa8e816010a5c46745b8bdfb8cfba2fe1
momak_bb <freeboy_bb@yahoo.co.uk>
2026-03-16 20:57:50 +0100

    publish v.1.2.17

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: Yu7i1skc */
+/* Manifest version: gDDNYbQq */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
